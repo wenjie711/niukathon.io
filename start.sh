e9cf4928597ed8400ddb16606de6dc062ac174a4
wenjie711 <411764740@qq.com>
2015-07-03 11:49:21 +0800

    add start script

---
+++ start.sh
@@ -0,0 +1,11 @@
+
+# File Name: start.sh
+# Author: Hat_Cloud
+# mail: jijianfeng529@gmail.com
+# Created Time: 2015-07-03 11:45
+#########################################################################
+#!/bin/bash
+export GOPATH=$GOPATH:/Users/wenjie/work/go/src/github.com/niukathon.io
+
+revel run niukathon.io
+
